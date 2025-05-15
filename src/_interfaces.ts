2a7dc97c7375c5052352ed7107fe19ef0680aef9
Luis Cortés <javiertinc@gmail.com>
2025-05-15 18:30:59 -0400

    Se corrige el formato de las interfaces

--- src/_interfaces.ts
+++ src/_interfaces.ts
@@ -1,23 +1,23 @@
 export interface iConfig {
     project: {
-        name: string;
-        version: string;
-        environment: string;
+        name: string,
+        version: string,
+        environment: string,
     },
     data: {
-        encrypt: boolean;
-        key: string;
+        encrypt: boolean,
+        key: string,
     },
     debug: {
-        cnsl: boolean;
-        log: boolean;
-        strg: boolean;
-        obsv: boolean;
+        cnsl: boolean,
+        log: boolean,
+        strg: boolean,
+        obsv: boolean
     },
-    log_quantity: number;
+    log_quantity: number
 }
 
 export interface iLog {
-    project: iConfig['project'];
-    logs: any[];
+    project: iConfig['project'],
+    logs: any[]
 }
